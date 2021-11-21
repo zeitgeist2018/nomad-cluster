a4b2f92fa7ee7ea2a38ae14573fde2d7cfd559b6
cristianlm <cristian.lobomartin@zooplus.com>
2021-11-21 21:52:50 +0100

    Foundation for node auto-provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,13 @@ resource tls_private_key ssh_key {
 resource local_file private_key_file {
   content  = tls_private_key.ssh_key.private_key_pem
   filename = "${local.output_folder}/private-key.pem"
+  file_permission = "600"
 }
 
 resource local_file public_key_file {
   content  = tls_private_key.ssh_key.public_key_pem
   filename = "${local.output_folder}/public-key.pem"
+  file_permission = "600"
 }
 
 # Root password
@@ -34,6 +36,13 @@ resource local_file root_password_file {
 
 # Instances
 
+resource linode_stackscript init_script {
+  label = "nomad-node-provision"
+  description = "Provisions a Nomad node"
+  script = file("provision.sh")
+  images = ["linode/ubuntu21.10"]
+}
+
 resource linode_instance nomad-nodes {
   count           = 2
   image           = "linode/ubuntu21.10"
@@ -41,6 +50,15 @@ resource linode_instance nomad-nodes {
   group           = "Nomad"
   region          = "ca-central"
   type            = "g6-nanode-1"
+  backups_enabled = false
+  private_ip = true
+  stackscript_id = linode_stackscript.init_script.id
   authorized_keys = [chomp(tls_private_key.ssh_key.public_key_openssh)]
   root_pass       = random_password.root_password.result
+}
+
+resource local_file ips_file {
+  count = length(linode_instance.nomad-nodes)
+  content = linode_instance.nomad-nodes[count.index].ip_address
+  filename = "${local.output_folder}/nomad-node-ip-${count.index}.txt"
 }
---
+++ terraform/provision.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Node provisioned" > /provision.txt
