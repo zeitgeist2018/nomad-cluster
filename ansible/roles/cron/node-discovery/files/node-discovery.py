3e52067cbb694f111b010f48007f494df358a171
cristianlm <cristian.lobomartin@zooplus.com>
2021-11-26 23:35:25 +0100

    WIP: Node discovery trys to self-heal node

--- ansible/roles/cron/node-discovery/files/node-discovery.py
+++ ansible/roles/cron/node-discovery/files/node-discovery.py
@@ -2,6 +2,7 @@ import copy
 import json
 import logging
 import os
+
 import requests
 
 logging.basicConfig(
@@ -31,12 +32,18 @@ def discover_nomad_nodes():
             valid_ip_addresses = [ip for ip in instance['ipv4'] if ip.startswith('192')]
             if status == 'running' and group == 'Nomad' and len(valid_ip_addresses) == 1:
                 host_ip = valid_ip_addresses[0]
-                is_server = is_a_server(host_ip)
-                logging.info(f'Discovered {"server" if is_server else "client"} node {label} with IP {host_ip}')
+                has_errors = False
+                is_server = False
+                try:
+                    is_server = is_a_server(host_ip)
+                    logging.info(f'Discovered {"server" if is_server else "client"} node {label} with IP {host_ip}')
+                except (Exception,):
+                    has_errors = True
                 nodes.append({
                     'label': label,
                     'ip': host_ip,
                     'is_server': is_server,
+                    'has_errors': has_errors,
                     'is_self': host_ip == own_ip
                 })
         return nodes
@@ -46,39 +53,44 @@ def discover_nomad_nodes():
 
 
 def is_a_server(ip):
-    req = requests.get(f'http://{ip}:4646/v1/agent/self')
-    if req.status_code == 200:
-        return req.json()['stats']['nomad']['server'] == "true"
+    try:
+        req = requests.get(f'http://{ip}:4646/v1/agent/self')
+        if req.status_code == 200:
+            return req.json()['stats']['nomad']['server'] == "true"
+    except (Exception,):
+        logging.error(f'Error retrieving agent information in {ip}')
     return False
+
+
+def write_nomad_config_file(config):
+    with open(nomad_config_file, 'w') as file:
+        json.dump(config, file, indent=2)
+
+
+def read_nomad_config_file():
+    with open(nomad_config_file, 'r', encoding="utf8") as f:
+        return json.load(f)
 
 
 def update_nomad_configuration(nomad_nodes):
     try:
-        f = open(nomad_config_file, 'r', encoding="utf8")
-        with f:
-            config = json.load(f)
-            config_backup = copy.deepcopy(config)
-            servers = list(filter(lambda node: node['is_server'] is True, nomad_nodes))
-            server_ips = list(map(lambda server: server['ip'], servers))
-            config['server']['bootstrap_expect'] = len(servers)
-            config['server']['server_join']['retry_join'] = server_ips
-            config['client']['server_join']['retry_join'] = server_ips
-            if config_diff(config_backup, config):
-                logging.info(f'Nomad configuration has changed')
-                with open(nomad_config_file_tmp, 'w') as tmp_file:
-                    json.dump(config, tmp_file, indent=2)
-                    service_action('nomad', 'stop')
-                    os.system(f'cp {nomad_config_file_tmp} {nomad_config_file}')
-                    service_action('nomad', 'start')
-                    return True
-            else:
-                logging.info('No changes detected')
-                print(config_backup)
-                print(config)
+        config = read_nomad_config_file()
+        config_backup = copy.deepcopy(config)
+        servers = list(filter(lambda node: node['is_server'] is True, nomad_nodes))
+        server_ips = list(map(lambda server: server['ip'], servers))
+        config['server']['server_join']['retry_join'] = server_ips
+        config['client']['server_join']['retry_join'] = server_ips
+        if config_diff(config_backup, config):
+            logging.info(f'Nomad configuration has changed')
+            write_nomad_config_file(config)
+            service_action('nomad', 'restart')
+            return True
+        else:
+            logging.info('No changes detected')
     except IOError as e:
         logging.error(f'Could not open file: {nomad_config_file}')
         logging.error(e)
-        return False
+    return False
 
 
 def config_diff(old_config, new_config):
@@ -86,11 +98,37 @@ def config_diff(old_config, new_config):
 
 
 def service_action(service_name, action):
-    if os.system(f'systemctl {action} {service_name}'):
+    if os.system(f'systemctl {action} {service_name}') == 0:
         logging.info(f'{service_name} service {action}')
+    else:
+        logging.error(f'Failed executing {action} on service {service_name}')
 
 
-nodes = discover_nomad_nodes()
-config_changed = update_nomad_configuration(nodes)
-if config_changed:
-    restart_nomad()
+def i_am_the_only_node_and_have_errors(nodes):
+    return len(nodes) == 1 and nodes[0]['is_self'] and nodes[0]['has_errors']
+
+
+def self_heal(nodes):
+    logging.info(f'Trying to self-heal...')
+    service_action('nomad', 'stop')
+    config = read_nomad_config_file()
+    config['server']['server_join']['retry_join'] = [nodes[0]['ip']]
+    config['client']['server_join']['retry_join'] = [nodes[0]['ip']]
+    service_action('nomad', 'start')
+
+
+def nomad_running_properly():
+    return os.system('nomad agent-info > /dev/null 2>&1') == 0
+
+
+def main():
+    nodes = discover_nomad_nodes()
+    if i_am_the_only_node_and_have_errors(nodes):
+        self_heal(nodes)
+    else:
+        if update_nomad_configuration(nodes):
+            service_action('nomad', 'restart')
+        elif not nomad_running_properly():
+            self_heal(nodes)
+
+main()
